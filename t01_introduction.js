7853a8dcc1237361fc9fc51e7ae0f5a72e1174e4
aquizs <24157lw@hvhs.school.nz>
2026-04-28 09:58:03 +1200

    task 1 introduction done

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,21 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+Name of task: Task 1
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("testing the stuf")
 
-// This is a single line comment
+//Variables
 
+
+/*************************
+Main Code
+ *************************/
+
+
+
+
+
+
+/**************************
+ Functions
+ **************************/
