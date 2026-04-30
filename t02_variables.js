36a1194db49b24fcb789d0cdf4ccd41e16a2de93
aquizs <24157lw@hvhs.school.nz>
2026-04-30 13:29:25 +1200

    fixing debugging still

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,25 @@
-/* Paste your code from task 1 here */
+/****************************
+Name of task: Task 2
+****************************/
+
+//Variables
+
+let username = "Liyan";
+let age = 15;
+let pocketMoney = 500;
+let year = 2026;
+
+/*************************
+Main Code
+ *************************/
+console.log("67")
+console.log("Hi " +username)
+console.log("As of " + year + " you are " + age +" years old")
+console.log("You have " +pocketMoney + " dollars")
+
+
+
+
+/**************************
+ Functions
+ **************************/
