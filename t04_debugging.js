a3532c95195aa469e7209058690edff5a2951bfc
aquizs <24157lw@hvhs.school.nz>
2026-05-01 15:01:29 +1200

    did debuging on HTML and java script now

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,21 +1,24 @@
+// Set up the size of the page.
+var length = 8;
+var width = 10;
 // calculate the area
-var area = length + width;
+var area = length * width;
+
+
 
 // calculate the perimeter
 var perimeter = 2 * length + 2 * width; 
 
-// Set up the size of the page.
-var length = 8;
-var width = 10;
+
 
 // Display the results
-console.log("Task 4- Debugging")
-console.log("A area of a piece of paper with a length of ", + length +", and a width of " ,+ width + " is ", + area)
-console.log("The same piece of paper has a perimeter of " + perimeter)
+console.log("Task 4-Debugging");
+console.log("A area of a piece of paper with a length of ", + length +" and a width of " + width + " is " + area);
+console.log("The same piece of paper has a perimeter of " + perimeter);
 
 // Check, does this result look sensible
-if (perimeter > area){
-    console.log("Yay, your area is larger, this is right for this example")
+if (perimeter < area){
+    console.log("Yay, your area is larger, this is right for this example");
 } else {
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
 }
--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,30 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Name of task: Task 3
+****************************/
+
+//Variables
+
+let username = "Liyan";
+let age = 15;
+let pocketMoney = 500;
+let year = 2026;
+
+/*************************
+Main Code
+ *************************/
+console.log("67")
+console.log("Hi " +username)
+console.log("As of " + year + " you are " + age +" years old")
+console.log("In 10 years you will be " ,+ age+10 +" years old")
+console.log("You have " +pocketMoney + " dollars")
+console.log("You spend half of your money, now you have" ,+ pocketMoney/2)
+console.log("Then you get $3, now you have" ,+ pocketMoney+3)
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+OUTPUT.innerHTML = "<h2>This is now saying Hi liyan</h2>";
+
+
+
+/**************************
+ Functions
+ **************************/
