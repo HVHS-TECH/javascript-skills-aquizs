2982bd8f27f2ee63922eb8667498c7d16657a2ba
aquizs <24157lw@hvhs.school.nz>
2026-05-12 09:56:45 +1200

    working on button not working bruh

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -34,6 +34,18 @@ OUTPUT.innerHTML += "<p>Then you get $3, now you have " + (pocketMoney+3) + "</p
 /**************************
  Functions
  **************************/
+
+function start document.getElementById("JavaScriptOutput") {
+OUTPUT.innerHTML = "<h2>This is now saying Hi Liyan</h2>";
+OUTPUT.innerHTML += "<p>67</h2>";
+OUTPUT.innerHTML += "<p>Hi " + username + "</p>";
+OUTPUT.innerHTML += "<p>As of " + year + " you will be " + age + " years old</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be " + (age + 10) + " years old</p>";
+OUTPUT.innerHTML += "<p>You will have 500 dollars</h2>";
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have "+ pocketMoney/2 + "</p>";
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + (pocketMoney+3) + "</p>";
+}
+
 function displayWelcome (_name){
     OUTPUT.innerHTML += "<h2> <br>Welcome to the shop</br> </h2>";
 }
