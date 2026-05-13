61a93fa7d5e347ff703f6152e8aa2d8b3751a111
aquizs <24157lw@hvhs.school.nz>
2026-05-13 12:28:32 +1200

    this task sucks task 9

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -35,7 +35,7 @@ OUTPUT.innerHTML += "<p>Then you get $3, now you have " + (pocketMoney+3) + "</p
  Functions
  **************************/
 
-function start document.getElementById("JavaScriptOutput") {
+function start = document.getElementById("JavaScriptOutput") {
 OUTPUT.innerHTML = "<h2>This is now saying Hi Liyan</h2>";
 OUTPUT.innerHTML += "<p>67</h2>";
 OUTPUT.innerHTML += "<p>Hi " + username + "</p>";
@@ -44,6 +44,11 @@ OUTPUT.innerHTML += "<p>In 10 years you will be " + (age + 10) + " years old</p>
 OUTPUT.innerHTML += "<p>You will have 500 dollars</h2>";
 OUTPUT.innerHTML += "<p>You spend half of your money, now you have "+ pocketMoney/2 + "</p>";
 OUTPUT.innerHTML += "<p>Then you get $3, now you have " + (pocketMoney+3) + "</p>";
+}
+
+function start (_name, _price){
+    displayWelcome
+    displayProduct
 }
 
 function displayWelcome (_name){
