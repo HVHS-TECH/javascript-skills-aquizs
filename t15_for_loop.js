fc1f5d2fe316bd4396dbe1d03046a78e0da211ca
aquizs <24157lw@hvhs.school.nz>
2026-05-27 12:29:01 +1200

    on task 16

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -10,10 +10,32 @@ console.log("testing the stuf")
 /*************************
 Main Code
  *************************/
-function (){
-for (let i=10; i>=0; i--){
-   OUTPUT.innerHTML("current count: "+i);
+  function getNumber(){
+    const NUMBER_FIELD = document.getElementById("numberField");
+    let count = Number(NUMBER_FIELD.value);
+    let lyrics = "";
+
+for (let i=count; i>=1; i--){
+  if (i===1) {
+    lyrics += "1 bottle of coke on the wall, 1 bottle of coke.<br>";
+    lyrics += "If one of those bottles should happen to fall, no more bottles of coke on the wall.<br><br>";
+  }
+
+else {
+  lyrics += i + " bottles of coke on the wall, " + i + " bottles of coke.<br>";
+  lyrics += "If one of those bottles should happen to fall, " + (i - 1);
+
+  if (i - 1 === 1) {
+    lyrics +=" bottle";
+  } else {
+    lyrics += "bottles";
+  }
+
+lyrics += " of coke on the wall<br>";
+
  }
+}
+OUTPUT.innerHTML = lyrics;
 }
 
  const OUTPUT = document.getElementById("JavaScriptOutput");
--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,23 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Task 16
+****************************/
+
+//Variables
+
+
+/*************************
+Main Code
+ *************************/
+ function getList(){
+    const LIST_FIELD = document.getElementById("listField");
+    let list = LIST_FIELD.value;
+
+let shoppingArray = ["victors laptop", "adams pc", "leos running shoes", "abhik"];
+OUTPUT.innerHTML = shoppingArray
+ }
+
+/**************************
+ Functions
+ **************************/
+
+ 
